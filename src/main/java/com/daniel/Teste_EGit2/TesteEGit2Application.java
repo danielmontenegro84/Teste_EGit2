d5e76fcbddfbc5dd0074c630e7d05789719e3d67
Daniel <dmontenegro84@gmail.com>
2025-03-15 23:25:15 -0300

    Primeiro commit

---
+++ src/main/java/com/daniel/Teste_EGit2/TesteEGit2Application.java
@@ -0,0 +1,13 @@
+package com.daniel.Teste_EGit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteEGit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteEGit2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/Teste_EGit2/TesteEGit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.Teste_EGit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteEGit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
